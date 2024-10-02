b276db3634a595b384070a7b41a5f0b1b067c270
RafaRocha89 <rafaelamattosrocharj01@gmail.com>
2024-10-01 21:21:06 -0300

    Add files via upload

--- AULA_01/exemplo_04.py
+++ AULA_01/exemplo_04.py
@@ -1,7 +1,12 @@
 # Calcular a idade de uma pessoa a partir do ano de nascimento
+import datetime
 num = int(input("Informe o Ano de nascimento: "))
 nome = input("Informe o seu nome: ")
-result = 2024 - num
+data_atual = datetime.date.today()
+dia_atual = data_atual.day
+mes_atual = data_atual.month
+ano_atual = data_atual.year
+result = ano_atual - num
 print("Sr(a)", nome, "a sua idade é: ",result,"anos.")
 
 
